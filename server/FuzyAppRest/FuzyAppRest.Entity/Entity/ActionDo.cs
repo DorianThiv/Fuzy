cb0540c543c896054b63df6732abb61b2745ccbf
Dorian Th <dorianth@orange.fr>
2021-04-09 14:55:28 +0200

    Initial commit

---
+++ server/FuzyAppRest/FuzyAppRest.Entity/Entity/ActionDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzyAppRest.Entity.Entity
+{
+    public class ActionDo : BaseEntity
+    {
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Description { get; set; }
+        public int ParentId { get; set; }
+    }
+}
